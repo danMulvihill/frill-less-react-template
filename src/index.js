76bf772883f28708edf416ffeefe48e374b8f14a
Dan Mulvihill <dmulvihill3@gmail.com>
2020-08-01 09:34:56 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const welcome = " Start your react App"
+
+ReactDOM.render(<p>{welcome}</p>, document.getElementById('root'));
+
